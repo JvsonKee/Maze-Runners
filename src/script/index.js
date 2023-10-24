8f2379a094ed78302d3d978c573f9e02bde9900a
MatthiasKee <matthias.kee@ucalgary.ca>
2023-10-23 18:41:47 -0600

    maze displays correctly

--- src/script/index.js
+++ src/script/index.js
@@ -7,17 +7,62 @@ let secondPlayerScore = 0;
  */
 let specialItemsArray = new Array(3);
 
-/**
- * maze array
- * will be used as the map
- */
-const maze = []
+const maze = [
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,1],
+    [1,0,1,1,0,0,0,0,0,0,0,1,0,1,1,1,1,0,1,0,1,1,1,1,1,0,1,1,1],
+    [1,0,1,1,1,0,1,1,1,1,1,1,0,1,1,1,1,0,1,0,0,0,0,1,1,0,1,1,1],
+    [1,0,0,1,1,0,0,1,0,1,1,0,0,0,0,1,1,0,1,1,1,1,0,1,1,0,1,1,1],
+    [1,1,0,1,1,1,0,0,0,1,1,1,1,1,0,1,1,0,0,0,0,1,0,1,1,0,1,1,1],
+    [1,0,0,1,0,1,0,1,1,1,1,0,0,0,0,0,1,1,0,1,0,0,0,1,1,0,1,1,1],
+    [1,0,1,1,0,1,0,0,0,0,0,0,1,1,1,1,1,1,0,1,1,0,1,1,1,0,1,1,1],
+    [2,0,0,0,0,0,0,1,1,1,1,0,1,1,1,1,1,1,0,1,1,0,1,0,0,0,1,1,1],
+    [1,1,0,1,1,1,1,1,0,0,0,0,1,1,1,0,0,0,0,1,1,0,1,0,1,0,0,0,3],
+    [1,1,0,1,0,0,0,1,1,1,0,1,1,1,1,0,1,1,0,1,1,0,1,0,1,1,1,1,1],
+    [1,1,0,1,0,1,0,1,1,1,0,1,1,0,0,0,0,1,0,1,1,0,1,0,1,0,0,0,1],
+    [1,0,0,1,0,1,0,0,0,1,0,0,0,0,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1],
+    [1,0,1,1,0,1,0,1,0,1,1,0,1,1,1,0,0,0,0,0,0,0,1,0,0,0,1,0,1],
+    [1,0,0,0,0,1,0,1,0,1,1,1,1,0,0,0,1,1,1,1,1,0,1,1,1,1,1,0,1],
+    [1,1,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,1,1,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+]
 
-/**
- * function to generate the maze
- * need to generate the maze based of the defined "maze" variable
- */
-function generateMaze() {}
+generateMaze();
+
+function generateMaze() {
+    gameboard.style.gridTemplateColumns = `repeat(${maze.length}, 1fr)`;
+    gameboard.style.gridTemplateColumns = `repeat(${maze[0].length}, 1fr)`;
+
+    for (let i = 0; i < maze.length; i++) {
+        for (let j = 0; j < maze[i].length; j++) {
+            const div = document.createElement('div');
+            switch (maze[i][j]) {
+                case 0: 
+                    div.classList.add('path');
+                    gameboard.append(div);
+                    div.style.backgroundColor = "tan";
+                    break;
+                case 1:
+                    div.classList.add('tree');
+                    gameboard.append(div);
+                    div.style.backgroundColor = "#619259";
+                    break;
+                case 2:
+                    div.classList.add('player');
+                    gameboard.append(div);
+                    div.style.backgroundColor = "#67d9ff";
+                    break;
+                case 3: 
+                    div.classList.add('finish');
+                    gameboard.append(div);
+                    div.style.backgroundColor = "#ffd630";
+                    break;
+                default:
+                    break;
+            }
+        } 
+    }
+}
 
 /**
  * function for player movement
